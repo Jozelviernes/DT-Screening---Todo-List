30f9ded23811f7fa77e44c9fa3158e838a3825b5
Jozel Viernes <viernesjozel6@gmail.com>
2025-04-06 06:40:02 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+        d
+          
+            const taskInput = document.querySelector('.task-input');
+            const addButton = document.querySelector('.add-btn');
+            const taskList = document.querySelector('.task-list');
+            const emptyListMessage = document.querySelector('.empty-list');
+            const totalTasksElement = document.querySelector('.total-tasks');
+            const completedTasksElement = document.querySelector('.completed-tasks');
+    
+            let totalTasks = 0;
+            let completedTasks = 0;
+            
+         
